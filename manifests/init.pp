c11a01b7089fbf0fa57756eba1df7d8e03d9fc4b
root <root@f2c93468201c.mylabserver.com>
2021-10-18 13:37:49 +0000

    update

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,11 +9,19 @@ class apache (
   String $install_ensure,
   String $config_ensure,
   String $config_path,
+  Enum["running", "stopped"] $service_ensure,
+  String $service_name,
+  Boolean $service_enable,
+  String $vhosts_dir,
+  String $vhosts_owner,
+  String $vhosts_group,
 ) {
   contain apache::install
   contain apache::config
+  contain apache::service
 
   Class['::apache::install']
   -> Class['::apache::config']
+  ~> Class['::apache::service']
 }
 
